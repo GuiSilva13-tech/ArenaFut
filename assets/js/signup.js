d9f52ca0a2c0ba34b595a0b7c2118047d19ed997
Guilherme Henrique <guilhermehoxz@gmail.com>
2024-06-18 11:16:43 -0300

    criando validação para e-mail

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -36,16 +36,19 @@ nome.addEventListener('keyup', () => {
 })
 
 usuario.addEventListener('keyup', () => {
-  if(usuario.value.length <= 4){
-    labelUsuario.setAttribute('style', 'color: red')
-    labelUsuario.innerHTML = 'Usuário *Insira no minimo 5 caracteres'
-    usuario.setAttribute('style', 'border-color: red')
-    validUsuario = false
+  // Expressão regular para validar e-mail
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (!emailRegex.test(usuario.value)) {
+    labelUsuario.setAttribute('style', 'color: red');
+    labelUsuario.innerHTML = 'Insira um e-mail válido';
+    usuario.setAttribute('style', 'border-color: red');
+    validUsuario = false;
   } else {
-    labelUsuario.setAttribute('style', 'color: green')
-    labelUsuario.innerHTML = 'Usuário'
-    usuario.setAttribute('style', 'border-color: green')
-    validUsuario = true
+    labelUsuario.setAttribute('style', 'color: green');
+    labelUsuario.innerHTML = 'Usuário';
+    usuario.setAttribute('style', 'border-color: green');
+    validUsuario = true;
   }
 })
 
@@ -130,7 +133,44 @@ btnConfirm.addEventListener('click', ()=>{
   }
 })
 
+function validarSenhaForca(){
+	var senha = document.getElementById('senhaForca').value;
+	var forca = 0;
 
+	if((senha.length >= 4) && (senha.length <= 7)){
+		forca += 10;
+	}else if(senha.length > 7){
+		forca += 25;
+	}
 
-  
-  
+	if((senha.length >= 5) && (senha.match(/[a-z]+/))){
+		forca += 10;
+	}
+
+	if((senha.length >= 6) && (senha.match(/[A-Z]+/))){
+		forca += 20;
+	}
+
+	if((senha.length >= 7) && (senha.match(/[@#$%&;*]/))){
+		forca += 25;
+	}
+
+	if(senha.match(/([1-9]+)\1{1,}/)){
+		forca += -25;
+	}
+
+	mostrarForca(forca);
+}
+
+function mostrarForca(forca){
+
+	if(forca < 30 ){
+		document.getElementById("erroSenhaForca").innerHTML = "<span style='color: #ff0000'>Fraca</span>";
+	}else if((forca >= 30) && (forca < 50)){
+		document.getElementById("erroSenhaForca").innerHTML = "<span style='color: #FFD700'>Média</span>";
+	}else if((forca >= 50) && (forca < 70)){
+		document.getElementById("erroSenhaForca").innerHTML = "<span style='color: #7FFF00'>Forte</span>";
+	}else if((forca >= 70) && (forca < 100)){
+		document.getElementById("erroSenhaForca").innerHTML = "<span style='color: #008000'>Excelente</span>";
+	}
+}
